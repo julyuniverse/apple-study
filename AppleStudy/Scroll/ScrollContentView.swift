3bc4580b5e253835ee67440ff833478f0e7009d0
julyuniverse <julyuniverse@icloud.com>
2025-05-15 09:29:36 +0900

    Updated

--- AppleStudy/Scroll/ScrollContentView.swift
+++ AppleStudy/Scroll/ScrollContentView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 struct ScrollContentView: View {
     @State var phase: ScrollPhase?
     @State private var isNewlyVisibleItemId: SomeItem.ID? // 스크롤 영역에서 "보이기 시작한" 아이템의 ID
-    @State var firstItem: SomeItem?
-    @State var seletedDate: Date?
+    @State var selectedDate = Date()
+    @State var firstItemDate = Date()
+    var items: [SomeItem] = SomeItem.previewItem
     
     var body: some View {
         VStack {
@@ -19,15 +20,16 @@ struct ScrollContentView: View {
                 Text("phase: \(phase)")
             }
             DatePicker("날짜 선택", selection: Binding(
-                get: { seletedDate ?? Date() },
+                get: { selectedDate },
                 set: { newDate in
-                    seletedDate = newDate
-                    print("selected")
+                    selectedDate = newDate
+                    firstItemDate = newDate
+                    print("selected: \(String(describing: selectedDate))")
                 }
             ), displayedComponents: .date)
             ScrollView {
                 LazyVStack {
-                    ForEach(SomeItem.previewItem) { item in
+                    ForEach(items) { item in
                         SomeItemView(item)
                             .background {
                                 if isNewlyVisibleItemId == item.id {
@@ -35,12 +37,17 @@ struct ScrollContentView: View {
                                 }
                             }
                             .onScrollVisibilityChange(threshold: 0.1) { isVisible in
-                                print("[\(item.id)] isVisible: \(isVisible)  ")
+                                //                                print("[\(item.id)] isVisible: \(isVisible)")
                                 if isVisible {
                                     isNewlyVisibleItemId = item.id
                                     if let lastItem = SomeItem.previewItem.last, lastItem.id == item.id {
                                         print("last")
                                         print("more activities...")
+                                    }
+                                    if let firstItem = items.first, !Calendar.current.isDate(firstItem.date, inSameDayAs: firstItemDate), Calendar.current.isDate(item.date, inSameDayAs: firstItemDate) {
+                                        print("first")
+                                        print("more activities...")
+                                        // 데이터를 가져온 후 firstItemDate 날짜를 가져온 데이터에서 가장 큰 날짜로 설정하는 로직 필요함.
                                     }
                                 }
                             }
@@ -64,11 +71,10 @@ struct SomeItem: Identifiable {
         let calendar = Calendar.current
         let startDate = Date()  // 또는 특정 기준 날짜(DateComponents로 생성)
         return (0..<100)
-            .map { offset in
+            .map { offset in // 최신일자부터 과거일자 순으로 정렬(여기선 이미 offset=0이 최신)
                 let d = calendar.date(byAdding: .day, value: -offset, to: startDate)!
                 return SomeItem(id: offset, date: d)
             }
-            // 최신일자부터 과거일자 순으로 정렬(여기선 이미 offset=0이 최신)
     }()
 }
 
@@ -80,8 +86,14 @@ struct SomeItemView: View {
     }
     
     var body: some View {
-        Text("\(item.date) \(item.id)")
+        Text("\(item.id) \(formattedDate(item.date))")
     }
+}
+
+func formattedDate(_ date: Date) -> String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd"
+    return formatter.string(from: date)
 }
 
 #Preview {
